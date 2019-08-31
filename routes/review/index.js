7fd042e63d7bac08ea83d6173401e3ee7b8a31df
Jimin Yeon <gmini.y@gmail.com>
2019-09-01 01:27:54 +0900

    [feat] review delete backend

--- routes/review/index.js
+++ routes/review/index.js
@@ -2,4 +2,5 @@ const router = require('express').Router();
 const reviewController = require('./review-controller');
 
 router.post('/', reviewController.register);
+router.delete('/', reviewController.delete);
 module.exports = router;
--- routes/review/review-controller.js
+++ routes/review/review-controller.js
@@ -1,4 +1,5 @@
 const Review = require('../../model/review').Review;
+const DeletedReview = require('../../model/review').DeletedReview;
 
 module.exports = {
     register : async (request, response, next) => {
@@ -8,6 +9,19 @@ module.exports = {
             const review = await Review.create(info);
             await review.save();
             return response.status(200).send();
+        } catch(error) {
+            next(error);
+        }
+    },
+
+    delete : async (request, response, next) => {
+        try {
+            const reviewId = request.query.id;
+            const review = await Review.findById(reviewId);
+            const deletedReview = await new DeletedReview(review.toObject());
+            await deletedReview.save();
+            await review.remove();
+            response.status(200).send();
         } catch(error) {
             next(error);
         }
